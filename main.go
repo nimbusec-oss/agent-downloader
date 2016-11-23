efd630a9101f0700d01e3ee53e904c481ffd11c4
Christian Baumgartner <christian@nimbusec.com>
2016-11-23 11:47:36 +0100

    added headless mode

--- main.go
+++ main.go
@@ -10,16 +10,35 @@ import (
 	"github.com/cumulodev/nimbusec"
 )
 
+var api *nimbusec.API
+
 // api reference: https://kb.nimbusec.com/API/API#agents
 func main() {
 	key := flag.String("key", "", "API key")
 	secret := flag.String("secret", "", "API secret")
+	agentArch := flag.String("arch", "64bit", "64bit or 32bit")
+	agentOS := flag.String("os", "linux", "linux or windows")
+	agentVersion := flag.Int("version", 13, "version")
+	agentFormat := flag.String("format", "bin", "tar.gz | zip | bin")
+	headless := flag.Bool("headless", false, "true if you know what you do")
 	flag.Parse()
 
 	// setup nimbusec api
-	api, err := nimbusec.NewAPI(nimbusec.DefaultAPI, *key, *secret)
+	var err error
+	api, err = nimbusec.NewAPI(nimbusec.DefaultAPI, *key, *secret)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	// download agent if you don't need guidance
+	if *headless {
+		download(nimbusec.Agent{
+			OS:      *agentOS,
+			Arch:    *agentArch,
+			Version: *agentVersion,
+			Format:  *agentFormat,
+		})
+		return
 	}
 
 	// fetch available nimbusec agents
@@ -42,19 +61,29 @@ func main() {
 		log.Fatal("Index out of range")
 	}
 
-	// prepare download over api
-	b, err := api.DownloadAgent(agents[i])
+	err = download(agents[i])
 	if err != nil {
 		log.Fatal(err)
 	}
-	f, err := os.Create(fmt.Sprintf("nimbusagent-%s-%s-%d.%s", agents[i].OS, agents[i].Arch, agents[i].Version, agents[i].Format))
+}
+
+// downloads the agent as requested to the current folder
+func download(agent nimbusec.Agent) error {
+	fmt.Printf("Downloading nimbusagent-%s-%s-%d.%s", agent.OS, agent.Arch, agent.Version, agent.Format)
+	b, err := api.DownloadAgent(agent)
 	if err != nil {
-		log.Fatal(err)
+		return err
+	}
+	f, err := os.Create(fmt.Sprintf("nimbusagent-%s-%s-%d.%s", agent.OS, agent.Arch, agent.Version, agent.Format))
+	if err != nil {
+		return err
 	}
 
 	_, err = f.Write(b)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer f.Close()
+
+	return nil
 }
